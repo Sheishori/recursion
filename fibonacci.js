ca9559b406762c61bf2738bfdfe4ccc27ee1d315
Sheishori <sheishori@gmail.com>
2022-12-08 11:50:46 +0100

    Change iterative function to return an array

--- fibonacci.js
+++ fibonacci.js
@@ -15,9 +15,11 @@ function fibsRec(num) {
 	let n = Number(num);
 	if (isNaN(n)) return "Must be a number";
 	if (n < 0) return "Can't be below 0";
-	if (n === 0) return 0;
-	if (n === 1) return 1;
-	return fibs(n - 1) + fibs(n - 2);
+	if (n === 0) return [];
+	if (n === 1) return [0];
+	if (n === 2) return [0, 1];
+	const array = fibsRec(n - 1);
+	return [...array, array[n - 2] + array[n - 3]];
 };
 
 console.log(fibsRec(8));
